54e685ec257734dd6681c23823d6da567f902203
YungNewton <isaacnewtonahanmisi@gmail.com>
2025-05-18 20:55:10 +0100

    commit

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar'
+
+
+const HomePage = () => {
+  return (
+    <div className="bg-black text-white">
+      <Navbar />
+    </div>
+  )
+}
+
+export default HomePage
